d5ce520e270d2d9b106f527fd80803c9191760e9
Duha Alkurdi <alkurdiduha@gmail.com>
2023-12-21 18:24:46 -0500

    Initialization

---
+++ script1.py
@@ -0,0 +1,24 @@
+import seaborn as sns
+import seaborn.objects as so
+# seaborn has some built-in datasets
+anscombe = sns.load_dataset('anscombe')
+
+print(anscombe.head())
+
+# print(sns.__version__)
+print((
+    anscombe
+    .groupby('dataset')  # "groupby" to group the different rows based on a  parameter called dataset
+    .agg(['mean','std'])
+))   # The four datasets have similar summary statistics 
+
+(
+  so.Plot(anscombe,
+         x = 'x',
+         y = 'y')  
+    .add(so.Dot())
+    .facet('dataset', wrap = 2)  # To facet based on the datasets, and to create 2 columns  
+    # .show()
+    .save("./figures/plot.png", dpi = 200)  # To save the plot, disable the show()
+    # dpi = 200; stands for dots per inch
+)
